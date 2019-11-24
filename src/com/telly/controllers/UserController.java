18208c03909610dd1f63c3917955c5733403ddd8
Lilyliod <fl_ait_zerrouk@esi.dz>
2019-11-24 10:57:11 +0100

    create user

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -37,5 +37,28 @@ public class UserController {
 	public String showLogout() {
 		return "loggedout";
 	}
+	@RequestMapping("/createaccount")
+	public String createAccount(Model model, Principal principal) {
+
+		model.addAttribute("user", new User());
+
+		return "createaccount";
+	}
+
+	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
+	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+
+		if(result.hasErrors()) {
+			return "createaccount";
+		}
+
+		user.setAuthority("ROLE_USER");
+		user.setEnabled(true);
+
+		userService.create(user);
+
+		return "home";
+
+	}
 
 }
